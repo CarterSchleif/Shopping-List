b8e352aaa583a23e499792174cabc7b8d2fd381c
CarterSchleif <carter.schleif@gmail.com>
2018-05-21 18:26:05 -0500

    setup files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// var session = require('express-session');
+
+// create the app
+var app = express();
+var port = process.env.PORT || 5000;
+
+// Body parser middleware
+app.use(bodyParser.json()); // parses angular data
+
+// Serve back static files
+app.use(express.static('./server/public'));
+
+
+
+/** Listen **/
+app.listen(port, function(){
+    console.log("Listening on port:", port);
+ });
